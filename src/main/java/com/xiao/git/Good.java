46c8962cf4d77add128e6d7b58f24e55f203581f
xiao_global <helloWorld_global@163.com>
2019-05-27 18:10:29 +0800

    gitDemo1提交代码

--- src/main/java/com/xiao/git/Good.java
+++ src/main/java/com/xiao/git/Good.java
@@ -2,7 +2,7 @@ package com.xiao.git;
 
 public class Good {
 	public static void main(String[] args) {
-		System.out.println("gitDemo say: Hello World");
+		System.out.println("gitDemo1 say: Hello World");
 	}
 
 }
