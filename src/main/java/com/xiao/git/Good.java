ff452c1905d185da2d32c1116d79827365b9926c
xiao_global <helloWorld_global@163.com>
2019-05-28 11:25:52 +0800

    分支提交

--- src/main/java/com/xiao/git/Good.java
+++ src/main/java/com/xiao/git/Good.java
@@ -4,6 +4,7 @@ public class Good {
 	public static void main(String[] args) {
 		System.out.println("gitDemo1 say: Hello World");
 		System.out.println("gitDemo say: Hello World");
+		System.out.println("分支代码提交");
 	}
 
 }
