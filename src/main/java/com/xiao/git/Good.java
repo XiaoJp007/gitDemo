ac863e206a57fc5547c70399ef346fa26d5ab487
eclipse_user <eclipse_email@163.com>
2019-05-28 11:11:38 +0800

    分支代码提交

--- src/main/java/com/xiao/git/Good.java
+++ src/main/java/com/xiao/git/Good.java
@@ -1,8 +1,15 @@
 package com.xiao.git;
 
+/**
+ * 
+ * @title 
+ * @author 肖建鹏
+ * @date 2019年5月28日上午11:10:42
+ */
 public class Good {
 	public static void main(String[] args) {
 		System.out.println("gitDemo say: Hello World");
+		System.out.println("分支代码提交");
 	}
 
 }
